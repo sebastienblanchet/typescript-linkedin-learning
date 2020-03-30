9c13bdf42b12926db4959529674e316eb9d5a102
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:20:32 -0400

    Class definitions

--- app.ts
+++ app.ts
@@ -1,33 +1,47 @@
 /* From the last video...
 
-function TodoService() {
+function TodoServiceClass() {
     this.todos = [];
 }
 
-TodoService.prototype.getAll = function() {
+TodoServiceClass.prototype.getAll = function() {
     return this.todos;
 }
-
 */
 
-class TodoService {
-    
-    constructor(private todos: Todo[]) {
-    }
-    
-    getAll() {
-        return this.todos;
-    }
+// New to ES6
+//
+class TodoServiceClass {
+  // could also have just defined it here
+  /*more work but still
+  todos: Todo[] = [];
+
+  ctor ( {
+    this.todos = []
+  })
+   */
+
+  // passed in at defn
+  // add private or other access modifier
+  // DEFINEs and ASSIGNS
+  // OUTPUT is SAME
+  constructor(private todos: Todo[]) {
+  }
+
+  // way of defining a obj.prototype
+  getAll() {
+    return this.todos;
+  }
 }
 
 interface Todo {
-    name: string;
-    state: TodoState;
+  name: string;
+  state: TodoStateEnum;
 }
 
-enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+enum TodoStateEnum {
+  New = 1,
+  Active,
+  Complete,
+  Deleted
 }
