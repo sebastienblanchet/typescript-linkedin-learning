20c22d9c2027068f0e0c26f76beee6f271c9f08e
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:39:52 -0400

    Abstract class, AWESOME

--- app.ts
+++ app.ts
@@ -1,44 +1,51 @@
 interface Todo {
-    name: string;
-    state: TodoState;
+  name: string;
+  state: TodoState;
 }
 
 enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+  New = 1,
+  Active,
+  Complete,
+  Deleted
 }
 
+// yesssssssssssssssss
+// this is not a es6 feature!!!!~!!!!!!
 abstract class TodoStateChanger {
-    
-    constructor(private newState: TodoState) {
+
+  constructor(private newState: TodoState) {
+  }
+
+  // MUST be overwritten
+  abstract canChangeState(todo: Todo): boolean;
+
+  changeState(todo: Todo): Todo {
+    if (this.canChangeState(todo)) {
+      todo.state = this.newState;
     }
-    
-    abstract canChangeState(todo: Todo): boolean;
-    
-    changeState(todo: Todo): Todo {
-        if(this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-        
-        return todo;
-    }
-    
+
+    return todo;
+  }
+
 }
 
 class CompleteTodoStateChanger extends TodoStateChanger {
 
-    constructor() {
-        super(TodoState.Complete);
-    }
-    
-    canChangeState(todo: Todo): boolean {
-        return !!todo && (
-              todo.state == TodoState.Active
-           || todo.state == TodoState.Deleted
-        )
-    }
-    
+  // NO need to override the super ctor
+  // IF the abstract keywork is not provided
+  constructor() {
+    super(TodoState.Complete);
+  }
+
+  // no keywork required for override
+  canChangeState(todo: Todo): boolean {
+    // cannot use the base class since the abstract was defined
+    return !!todo && (
+      todo.state == TodoState.Active
+      || todo.state == TodoState.Deleted
+    )
+  }
+
 }
 
