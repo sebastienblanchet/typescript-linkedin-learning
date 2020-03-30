35b5acc064bce4bbd8feb1c651a1f5bfd9ae7e61
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:35:13 -0400

    Class inheritance, super

--- app.ts
+++ app.ts
@@ -1,45 +1,50 @@
 interface Todo {
-    name: string;
-    state: TodoState;
+  name: string;
+  state: TodoState;
 }
 
 enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+  New = 1,
+  Active,
+  Complete,
+  Deleted
 }
 
+// basic class defn
 class TodoStateChanger {
-    
-    constructor(private newState: TodoState) {
+
+  constructor(private newState: TodoState) {
+  }
+
+  canChangeState(todo: Todo): boolean {
+    return !!todo;
+  }
+
+  changeState(todo: Todo): Todo {
+    if (this.canChangeState(todo)) {
+      todo.state = this.newState;
     }
-    
-    canChangeState(todo: Todo): boolean {
-        return !!todo;
-    }
-    
-    changeState(todo: Todo): Todo {
-        if(this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-        
-        return todo;
-    }
-    
+
+    return todo;
+  }
+
 }
 
+// inheritance
 class CompleteTodoStateChanger extends TodoStateChanger {
 
-    constructor() {
-        super(TodoState.Complete);
-    }
-    
-    canChangeState(todo: Todo): boolean {
-        return super.canChangeState(todo) && (
-              todo.state == TodoState.Active
-           || todo.state == TodoState.Deleted
-        )
-    }
-    
+  constructor() {
+    // calls the ctor of the PARENT class
+    // defn: The super keyword is used to access and call functions on an object's parent.
+    super(TodoState.Complete);
+  }
+
+  canChangeState(todo: Todo): boolean {
+    // equivalent of calling paring class
+    // wraps the super class
+    return super.canChangeState(todo) && (
+      todo.state == TodoState.Active
+      || todo.state == TodoState.Deleted
+    )
+  }
 }
