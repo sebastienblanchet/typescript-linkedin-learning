cdc54f10eb96ad42727c3dcb415c964268ef0212
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 19:12:25 -0400

    Generic type constraints

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
-interface IHaveALength {
-    length: number;
+interface IHasALength {
+  length: number;
 }
 
-function totalLength<T extends IHaveALength>(x: T, y: T) {
-    var total: number = x.length + y.length;
-    return total;
+// Generic constraints
+function totalLength<T extends IHasALength>(x: T, y: T) {
+  var total: number = x.length + y.length;
+  return total;
 }
 
-class CustomArray<T> extends Array<T> {}
+class CustomArray<T> extends Array<T> { }
 
+// caveats, does not assure that IHasALength is same
 var length = totalLength([1, 2, 3], new CustomArray<number>())
