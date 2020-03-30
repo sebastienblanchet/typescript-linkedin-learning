8faac06201d6786dd3c3157dae6cd50d38f48d08
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 17:41:27 -0400

    Destructuring

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
-function countdown({ 
-    initial, 
-    final: final = 0, 
-    interval: interval = 1, 
-    initial: current 
+// pass in a destructed ref
+function countdown({
+  initial,
+  final: final = 0,
+  interval: interval = 1,
+  initial: current
 }) {
-    
-    while (current > final) {
-        console.log(current);
-        current -= interval;
-    }
+
+  while (current > final) {
+    console.log(current);
+    current -= interval;
+  }
 
 }
