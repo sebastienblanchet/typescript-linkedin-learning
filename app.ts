494b49a53bb45c371541e65a5bf44c9360ced694
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 17:53:58 -0400

    Intro to types

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-
+// explcit definition of return
+// an array of any type, so make sure that .length always defined
 function totalLength(x: any[], y: string): number {
-    var total: number = x.length + y.length;
-    return total;
+  var total: number = x.length + y.length;
+  return total;
 }
