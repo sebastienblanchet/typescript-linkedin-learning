571c1c19bfda2da45884a65baf7e2b6e78a1bf27
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:13:18 -0400

    Anonymous types

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
+// anonymous types
+// define an interface one line
 var todo: { name: string };
 
+// adding a new prop?
 todo = { age: 41 }
 
+// anonymous types, give anything that has an obj length
 function totalLength(x: { length: number }, y: { length: number }): number {
-    var total: number = x.length + y.length;
-    return total;
+  var total: number = x.length + y.length;
+  return total;
 }
