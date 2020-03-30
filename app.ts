3619ae208e5ebf019914bb1c222256e8c3a5f504
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:32:00 -0400

    Accessors get/set

--- app.ts
+++ app.ts
@@ -1,68 +1,72 @@
 class TodoService {
 
-    static lastId: number = 0;
+  static lastId: number = 0;
 
-    constructor(private todos: Todo[]) {
-    }
+  constructor(private todos: Todo[]) {
+  }
 
-    add(todo: Todo) {
-        var newId = TodoService.getNextId();
-    }
+  add(todo: Todo) {
+    var newId = TodoService.getNextId();
+  }
 
-    getAll() {
-        return this.todos;
-    }
+  getAll() {
+    return this.todos;
+  }
 
-    static getNextId() {
-        return TodoService.lastId += 1;
-    }
+  static getNextId() {
+    return TodoService.lastId += 1;
+  }
 }
 
 interface Todo {
-    name: string;
-    state: TodoState;
+  name: string;
+  state: TodoState;
 }
 
 enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+  New = 1,
+  Active,
+  Complete,
+  Deleted
 }
 
 class SmartTodo {
-    
-    _state: TodoState;
-    
-    name: string;
-    
-    get state() {
-        return this._state;
+
+  _state: TodoState;
+
+  name: string;
+
+  // accessors {get, set}
+  get state() {
+    // must always return
+    return this._state;
+  }
+
+  set state(newState) {
+
+    if (newState == TodoState.Complete) {
+
+      var canBeCompleted =
+        this.state == TodoState.Active
+        || this.state == TodoState.Deleted;
+
+      if (!canBeCompleted) {
+        throw "Todo must be Active or Deleted in order to be marked Completed"
+      }
     }
-    
-    set state(newState) {
-        
-        if(newState == TodoState.Complete) {
-            
-            var canBeCompleted = 
-                this.state == TodoState.Active
-                || this.state == TodoState.Deleted;
-                
-            if(!canBeCompleted) {
-                throw "Todo must be Active or Deleted in order to be marked Completed"
-            }
-        }
-        
-        this._state = newState;
-    }
-    
-    constructor(name: string) {
-        this.name = name;
-    }
+
+    this._state = newState;
+  }
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 var todo = new SmartTodo("Pick up drycleaning");
 
+// calls setter
 todo.state = TodoState.Complete;
 
+// calls getter
 todo.state
