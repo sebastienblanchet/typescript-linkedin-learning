623c1aa32b240d8e2182d494bb29dd7ed3f4cae5
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:50:27 -0400

    Generics/templates

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
+// GENERICS in C#
+// templates C++
+// cool
+// Use these to just make sure that all are same type
+// but can be ANY, so similar to that, know when to use
 function clone<T>(value: T): T {
-    let serialized = JSON.stringify(value);
-    return JSON.parse(serialized);
+  let serialized = JSON.stringify(value);
+  return JSON.parse(serialized);
 }
 
 clone('Hello!')
@@ -8,11 +13,11 @@ clone('Hello!')
 clone(123)
 
 var todo: Todo = {
-    id: 1,
-    name: 'Pick up drycleaning',
-    state: TodoState.Active
+  id: 1,
+  name: 'Pick up drycleaning',
+  state: TodoState.Active
 };
 
+// can give the function anything, it can handle it
 clone(todo)
-
 clone({ name: 'Jess' })
