aeb53cfdc58103a3b2b9e6957fa077d3b4b33068
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 17:55:49 -0400

    Union types

--- app.ts
+++ app.ts
@@ -1,16 +1,20 @@
-
+/*
+Union type ( {type1} | {type2} )
+ */
 function totalLength(x: (string | any[]), y: (string | any[])): number {
-    var total: number = x.length + y.length;
-    
-    x.slice(0)
-    
-    if(x instanceof Array) {
-        x.push('TypeScript')
-    }
-    
-    if(x instanceof String) {
-        x.substr(0)
-    }
-    
-    return total;
+
+  var total: number = x.length + y.length;
+
+  x.slice(0)
+
+  // type guard syntax, verify typ eof variable
+  if (x instanceof Array) {
+    x.push('TypeScript')
+  }
+
+  if (x instanceof String) {
+    x.substr(0)
+  }
+
+  return total;
 }
