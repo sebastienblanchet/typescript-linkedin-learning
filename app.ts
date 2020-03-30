8382abeb9e6f1a4c8c8bd8ce159dedaf6a77cd81
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 18:42:02 -0400

    Access modifiers and caveats....

--- app.ts
+++ app.ts
@@ -1,79 +1,83 @@
 interface Todo {
-    name: string;
-    state: TodoState;
+  name: string;
+  state: TodoState;
 }
 
 enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+  New = 1,
+  Active,
+  Complete,
+  Deleted
 }
 
 
 class TodoService {
 
-    private static _lastId: number = 0;
+  // showcase the access modifiers
+  // this is all very close to C#
+  private static _lastId: number = 0;
 
-    private get nextId() {
-        return TodoService.getNextId();
-    }
+  // ACCESS modifier MUST be the same for setter and getter shit
+  // will need to wrap this
+  private get nextId() {
+    return TodoService.getNextId();
+  }
 
-    private set nextId(nextId) {
-        TodoService._lastId = nextId - 1;
-    }
+  private set nextId(nextId) {
+    TodoService._lastId = nextId - 1;
+  }
 
-    constructor(private todos: Todo[]) {
-    }
+  constructor(private todos: Todo[]) {
+  }
 
-    add(todo: Todo) {
-        var newId = this.nextId;
-    }
+  add(todo: Todo) {
+    var newId = this.nextId;
+  }
 
-    private getAll() {
-        return this.todos;
-    }
+  private getAll() {
+    return this.todos;
+  }
 
-    static getNextId() {
-        return TodoService.lastId += 1;
-    }
+  static getNextId() {
+    return TodoService.lastId += 1;
+  }
 }
 
 
 abstract class TodoStateChanger {
 
-    constructor(protected newState: TodoState) {
+  constructor(protected newState: TodoState) {
+  }
+
+  abstract canChangeState(todo: Todo): boolean;
+
+  changeState(todo: Todo): Todo {
+    if (this.canChangeState(todo)) {
+      todo.state = this.newState;
     }
 
-    abstract canChangeState(todo: Todo): boolean;
-
-    changeState(todo: Todo): Todo {
-        if (this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-
-        return todo;
-    }
+    return todo;
+  }
 
 }
 
 class CompleteTodoStateChanger extends TodoStateChanger {
 
-    constructor() {
-        super(TodoState.Complete);
-    }
+  constructor() {
+    super(TodoState.Complete);
+  }
 
-    canChangeState(todo: Todo): boolean {
-        return !!todo && (
-            todo.state == TodoState.Active
-            || todo.state == TodoState.Deleted
-        )
-    }
+  canChangeState(todo: Todo): boolean {
+    return !!todo && (
+      todo.state == TodoState.Active
+      || todo.state == TodoState.Deleted
+    )
+  }
 
 }
 
 
 class SmartTodo {
-    constructor(public name: string) {
-    }
+  constructor(public name: string) {
+  }
 }
