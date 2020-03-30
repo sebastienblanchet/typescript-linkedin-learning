d02360fb85098118ccde14cc446f9e65abc22c88
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 17:48:54 -0400

    Basic js type overview

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-var animal = { 
-    name: "Fido",
-    species: "Dog",
-    age: 5,
-    speak: function() { 
-        console.log('Woof!'); 
-    }
+// ES5
+// string, num, bool, obj, undefined/null
+
+// Same as ES5
+var animal = {
+  name: "Fido",
+  species: "Dog",
+  age: 5,
+  speak: function () {
+    console.log('Woof!');
+  }
 }
 
