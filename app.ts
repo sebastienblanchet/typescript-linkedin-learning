5632e405a6b40a724382f3f0d0f1f7f7d09efc9d
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 17:44:24 -0400

    Spread op

--- app.ts
+++ app.ts
@@ -1,29 +1,33 @@
+// example of vargs using the ..., SRPEAD op
 function calculate(action, ...values) {
-    var total = 0;
+  var total = 0;
 
-    for (var value of values) {
-        switch(action) {
-            case 'add':
-                total += value;
-                break;
-            
-            case 'subtract':
-                total -= value;
-                break;
-        }
+  for (var value of values) {
+    switch (action) {
+      case 'add':
+        total += value;
+        break;
+      case 'subtract':
+        total -= value;
+        break;
+      default:
+        break;
     }
+  }
 
-    return total;
+  return total;
 }
 
-calculate('add', 1,2,3,4,5)
+calculate('add', 1, 2, 3, 4, 5)
 
 
 var source = [3, 4, 5]
-var target = [ 1, 2, ...source, 6, 7 ]
+// using it in array
+var target = [1, 2, ...source, 6, 7]
 
 
-var list = [1,2,3]
-var toAdd = [4,5,6];
+var list = [1, 2, 3]
+var toAdd = [4, 5, 6];
 
+// append to end cool
 list.push(...toAdd)
