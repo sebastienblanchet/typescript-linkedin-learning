f411cf7466d5685398e22e184014629e1d90df68
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-31 09:58:36 -0400

    Property decorators

--- TodoApp.ts
+++ TodoApp.ts
@@ -3,69 +3,69 @@ import TodoService, { ITodoService } from './TodoService';
 import TodoListComponent from './TodoListComponent';
 
 export class TodoApp {
-    
-    private todoService: ITodoService;
-    private todoList: TodoListComponent;
 
-    constructor(el, todos) {
+  private todoService: ITodoService;
+  private todoList: TodoListComponent;
 
-        this.todoService = new TodoService(todos);
-        this.initialize(el);
+  constructor(el, todos) {
+
+    this.todoService = new TodoService(todos);
+    this.initialize(el);
+  }
+
+  addTodo(todoName) {
+    try {
+      this.todoService.add(todoName);
+    } catch (x) {
+      console.error(x)
     }
 
-    addTodo(todoName) {
-        try {
-            this.todoService.add(todoName);
-        } catch(x) {
-            console.error(x)
-        }
-        
-        this.renderTodos();
-    }
+    this.renderTodos();
+  }
 
-    clearCompleted() {
-        this.todoService.clearCompleted();
-        this.renderTodos();
-    }
+  clearCompleted() {
+    this.todoService.clearCompleted();
+    this.renderTodos();
+  }
 
-    toggleTodoState(todoId) {
-        this.todoService.toggle(todoId);
-        this.renderTodos();
-    }
+  toggleTodoState(todoId) {
+    this.todoService.toggle(todoId);
+    this.renderTodos();
+  }
 
-    renderTodos() {
-        var todos = this.todoService.getAll();
-        this.todoList.render(todos);
-    }
+  renderTodos() {
+    var todos = this.todoService.getAll();
+    this.todoList.render(todos);
+  }
 
-    initialize(el) {
+  initialize(el) {
 
-        var _this = this;
+    var _this = this;
 
-        var addTodoFormEl = el.getElementsByClassName('add-todo')[0],
-            addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0],
-            todoListEl = el.getElementsByClassName('todo-list')[0],
-            clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
+    var addTodoFormEl = el.getElementsByClassName('add-todo')[0],
+      addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0],
+      todoListEl = el.getElementsByClassName('todo-list')[0],
+      clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
 
-        addTodoFormEl.addEventListener('submit', function(evnt) {
-            _this.addTodo(addTodoNameEl.value)
-            addTodoNameEl.value = '';
-            evnt.preventDefault();
-        });
+    addTodoFormEl.addEventListener('submit', function (evnt) {
+      _this.addTodo(addTodoNameEl.value)
+      addTodoNameEl.value = '';
+      evnt.preventDefault();
+    });
 
-        todoListEl.addEventListener('todo-toggle', function(evnt) {
-            var todoId = evnt.details.todoId;
-            _this.todoService.toggle(todoId);
-            _this.renderTodos();
-        });
+    todoListEl.addEventListener('todo-toggle', function (evnt) {
+      var todoId = evnt.details.todoId;
+      _this.todoService.toggle(todoId);
+      _this.renderTodos();
+    });
 
-        clearCompletedEl.addEventListener('click', function() {
-            _this.clearCompleted();
-        });
+    clearCompletedEl.addEventListener('click', function () {
+      _this.clearCompleted();
+    });
 
-        this.todoList = new TodoListComponent(todoListEl);
+    this.todoList = new TodoListComponent(todoListEl);
 
-        this.renderTodos();
-    }
+    this.renderTodos();
+  }
 
 }
--- Validators.ts
+++ Validators.ts
@@ -3,12 +3,12 @@ import { Todo, TodoState } from './Model';
 @validatable
 export class ValidatableTodo implements Todo {
 
-    id: number;
+  id: number;
 
-    @required
-    name: string;
+  @required
+  name: string;
 
-    state: TodoState;
+  state: TodoState;
 }
 
 export interface ValidatableTodo extends IValidatable {
@@ -16,62 +16,63 @@ export interface ValidatableTodo extends IValidatable {
 
 
 export interface IValidatable {
-    validate(): IValidationResult[];
+  validate(): IValidationResult[];
 }
 
 export interface IValidationResult {
-    isValid: boolean;
-    message: string;
-    property?: string;
+  isValid: boolean;
+  message: string;
+  property?: string;
 }
 
 export interface IValidator {
-    (instance: Object): IValidationResult;
+  (instance: Object): IValidationResult;
 }
 
 export function validate(): IValidationResult[] {
-    let validators: IValidator[] = [].concat(this._validators),
-        errors: IValidationResult[] = [];
+  let validators: IValidator[] = [].concat(this._validators),
+    errors: IValidationResult[] = [];
 
-    for (let validator of validators) {
-        
-        let result = validator(this);
-        
-        if(!result.isValid) {
-            errors.push(result);
-        }
-        
+  for (let validator of validators) {
+
+    let result = validator(this);
+
+    if (!result.isValid) {
+      errors.push(result);
     }
-    
-    return errors;
+
+  }
+
+  return errors;
 }
 
 export function validatable(target: Function) {
-    
-    target.prototype.validate = validate;
-    
+
+  target.prototype.validate = validate;
+
 }
 
+// property decorators
 export function required(target: Object, propertyName: string) {
-    
-    let validatable = <{ _validators: IValidator[] }>target,
-        validators = (validatable._validators || (validatable._validators = []));
-        
-    validators.push(function (instance) {
-        
-        let propertyValue = instance[propertyName],
-            isValid = propertyValue != undefined;
-            
-        if( typeof propertyValue === 'string' ) {
-            isValid = propertyValue && propertyValue.length > 0;
-        }
-        
-        return {
-            isValid,
-            message: `${propertyName} is required`,
-            property: propertyName
-        }
-        
-    })
-    
+
+  let validatable = <{ _validators: IValidator[] }>target,
+    validators = (validatable._validators || (validatable._validators = []));
+
+  validators.push(function (instance) {
+
+    let propertyValue = instance[propertyName],
+      isValid = propertyValue != undefined;
+
+    if (typeof propertyValue === 'string') {
+      isValid = propertyValue && propertyValue.length > 0;
+    }
+
+    return {
+      isValid,
+      message: `${propertyName} is required`,
+      property: propertyName
+    }
+
+  })
+
 }
