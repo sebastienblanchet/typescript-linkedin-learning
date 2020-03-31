e965e14aa4dbffacafeea8fb5a4d6f9c32eab1f1
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-31 09:13:15 -0400

    External module approach using ES6

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
+// This is using the ES6 syntax
+// allows you to define aliases in the statement
 import { Todo as TodoTask, TodoState } from './model';
+// import * as Model from '.model' everything
 import './jQuery'
 
 let todo: TodoTask;
