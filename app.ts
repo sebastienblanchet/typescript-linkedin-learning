fe539228f3f348bebe3a3ffe75f2a7eabf4f8334
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-31 09:17:27 -0400

    Module loaders

--- app.ts
+++ app.ts
@@ -1,17 +1,21 @@
 import { Todo as TodoTask, TodoState } from './model';
 import { TodoService } from './DataAccess';
 
+// NEED A MODULE LOADER
+// dependent on the FILE type
+// System.js is used for ES6 as loader
+// need to wait for browser compability
 
 let service = new TodoService([]);
 
 service.add({
-    id: 1,
-    name: 'Pick up drycleaning',
-    state: TodoState.New
+  id: 1,
+  name: 'Pick up drycleaning',
+  state: TodoState.New
 });
 
 let todos = service.getAll();
 
 todos.forEach(todo =>
-    console.log(`${todo.name} [${TodoState[todo.state]}]`) 
+  console.log(`${todo.name} [${TodoState[todo.state]}]`)
 )
